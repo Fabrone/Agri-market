a21c00356d34cd55f97160f04f79716e82258ec2
Fabrone <lubangafabron@gmail.com>
2025-01-09 12:34:30 +0300

    Updated project files and removed unused screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,20 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:agri_market/splashscreen.dart';
-//import 'package:agri_market/screens/authentication/email_auth_screen.dart';
-//import 'package:agri_market/screens/authentication/email_verification_screen.dart';
-//import 'package:agri_market/screens/authentication/reset_password_screen.dart';
-//import 'package:agri_market/screens/categories/category_list.dart';
-//import 'package:agri_market/screens/categories/subCat_screen.dart';
-//import 'package:agri_market/screens/home_screen.dart';
-//import 'package:agri_market/screens/location_screen.dart';
-//import 'package:agri_market/screens/login_screen.dart';
-//import 'package:agri_market/screens/authentication/phoneauth_screen.dart';
-//import 'package:agri_market/screens/main_screen.dart';
-//import 'package:agri_market/screens/playlist_screen.dart';
-//import 'package:agri_market/screens/product_details_screen.dart';
-//import 'package:agri_market/screens/sellitems/seller_category_list.dart';
-//import 'package:agri_market/screens/sellitems/seller_subCat.dart';
-//import 'package:agri_market/screens/splash_screen.dart';
-//import 'package:agri_market/forms/user_review_screen.dart';
-//import 'package:agri_market/forms/forms_screen.dart';
-//import 'package:agri_market/forms/tools_form.dart';
-//import 'package:agri_market/provider/cat_provider.dart';
-//import 'package:agri_market/provider/product_provider.dart';
-import 'package:provider/provider.dart';
+import 'firebase_options.dart';
+import 'package:logging/logging.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(
-    MultiProvider(
-      providers: const [
-        //Provider(create: (_) => CategoryProvider()),
-        //Provider(create: (_) => ProductProvider()),
-      ],
-      child: const MyApp(),
-    ),
-  );
+  try {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  } catch (e) {
+    Logger.root.severe('Error initializing Firebase: $e');
+  }
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -42,13 +22,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: Colors.indigo,
-        fontFamily: 'RobotoSlab-Regular',
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      builder: (_, child) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Agri-Market',
+        theme: ThemeData(
+          useMaterial3: true,
+          primaryColor: const Color.fromARGB(255, 49, 137, 52),
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 49, 137, 52),
+            onPrimary: Colors.white,
+            onSecondary: const Color.fromARGB(255, 218, 255, 220),
+          ),
+          buttonTheme: const ButtonThemeData(
+            buttonColor: Color.fromARGB(255, 54, 140, 57),
+          ),
+          fontFamily: 'Poppins',
+        ),
+        home: const SplashScreen(),
       ),
-      home: const SplashScreen(), // Set the HomeScreen as the initial screen
     );
   }
 }
